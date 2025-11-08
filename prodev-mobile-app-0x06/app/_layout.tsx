40bdaa4a1bc2dc46131933b444972fb2f2c5704c
Enock <enockzy54@gmail.com>
2025-11-08 15:48:16 +0300

    Styling with NativeWind (TailwindCSS)

--- prodev-mobile-app-0x06/app/_layout.tsx
+++ prodev-mobile-app-0x06/app/_layout.tsx
@@ -3,9 +3,11 @@ import "../styles/global.css";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{
-      headerShown: false,
-    }}>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Stack.Screen name="/" />
       <Stack.Screen name="join" />
       <Stack.Screen name="signin" />
